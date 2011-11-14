dfed8d8554f78677c828ef39f9c622beaf445632
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2011-11-14 21:38:57 +0100

    Clean up project definition.

--- project.clj
+++ project.clj
@@ -1,15 +1,36 @@
 (defproject benki "0.0.0"
   :description "The Benkard Family Hub"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure.contrib/standalone "1.3.0-SNAPSHOT"]
+
+                 ;; Clojure Contrib
+                 [org.clojure.contrib/repl-utils "1.3.0-SNAPSHOT"]
+                 [org.clojure/algo.generic "0.1.0-SNAPSHOT"]
+                 [org.clojure/algo.monads "0.1.0"]
+                 [org.clojure/core.logic "0.6.5"]
                  [org.clojure/core.match "0.2.0-SNAPSHOT"]
-                 [org.clojure/core.logic "0.6.6-SNAPSHOT"]
-                 [org.clojure/core.unify "0.5.2-SNAPSHOT"]
+                 [org.clojure/core.unify "0.5.1"]
+                 [org.clojure/data.codec "0.1.0"]
                  [org.clojure/data.finger-tree "0.0.2-SNAPSHOT"]
+                 ;;[org.clojure/data.xml.root "0.0.1-SNAPSHOT" :type "pom"]
+                 [org.clojure/data.zip "0.1.0"]
+                 [org.clojure/java.jdbc "0.1.1"]
+                 [org.clojure/java.data "0.0.1-SNAPSHOT"]
+                 [org.clojure/java.jmx "0.2-SNAPSHOT"]
+                 [org.clojure/math.combinatorics "0.0.3-SNAPSHOT"]
+                 [org.clojure/math.numeric-tower "0.0.2-SNAPSHOT"]
+                 [org.clojure/test.generative "0.1.3"]
+                 ;;[org.clojure/test.benchmark "0.1.0-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.2.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/tools.macro "0.1.1"]
+                 [org.clojure/tools.namespace "0.1.0"]
+                 [org.clojure/tools.nrepl "0.0.5"]
+                 [org.clojure/tools.trace "0.7.1"]
+                 ;;[org.clojure.contrib/standalone "1.3.0-SNAPSHOT"]
 
                  ;; Web app utilities
                  [ring "1.0.0-RC1"]
-                 [noir "1.2.0"]
+                 [noir "1.2.2-SNAPSHOT"]
                  [hiccup "0.3.7"]
                  [cssgen "0.2.5-SNAPSHOT"]
                  ;;[clj-http "0.2.3"]
@@ -21,7 +42,7 @@
                  [org.clojure/java.jdbc "0.1.1"]
 
                  ;; Other databases
-                 [com.sleepycat/je "4.0.92"]
+                 [com.sleepycat/je "4.1.10"]  ;4.0.96 if the Oracle Maven repo scares you
                  ;;[com.h2database/h2 "1.3.161"]
                  ;;[hypergraphdb/hypergraphdb "1.1"]     ;no Maven artifact
                  ;;[org.clojars.gregburd/cupboard "1.0.0-SNAPSHOT"]
@@ -50,7 +71,12 @@
                  "openid4java-releases"
                  {:url "http://oss.sonatype.org/content/repositories/openid4java-releases/"
                   :snapshots false},
-                 "clojars" "http://clojars.org/repo/"}
+                 "clojars"
+                 {:url "http://clojars.org/repo/"
+                  :snapshots true},
+                 "oracle"
+                 {:url "http://download.oracle.com/maven/"
+                  :snapshots false}}
   :source-path "src"
   ;;:jvm-opts ["-Xms32m"]
   :main mulk.benki.main)
