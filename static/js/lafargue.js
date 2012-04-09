0691218f525e2546448613863c8bd684f80c297f
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-04-10 00:50:04 +0200

    Lafargue: Make live update script more resilient to network failures.

--- static/js/lafargue.js
+++ static/js/lafargue.js
@@ -1,9 +1,19 @@
 jQuery(function($) {
     if (WebSocket) {
         var websocket_base = $('head').attr('data-websocket-base');
-        var socket = new WebSocket(websocket_base + '/lafargue/events');
-        socket.onmessage = function(event) {
-            $('.lafargue-list').prepend(event.data);
+        var open_socket = function() {
+            var socket = new WebSocket(websocket_base + '/lafargue/events');
+            socket.onmessage = function(event) {
+                $('.lafargue-list').prepend(event.data);
+            };
+            var reconnect = function() {
+                window.setTimeout(function() {
+                    open_socket();
+                    socket.close();
+                }, 2000);
+            };
+            socket.onclose = reconnect;
         };
+        open_socket();
     }
 });
