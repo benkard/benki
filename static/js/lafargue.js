e5d1c5b63b348a0fd657f99ca8ba4d055031202a
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-04-09 21:28:51 +0200

    Add missing file static/js/lafargue.js.

--- static/js/lafargue.js
+++ static/js/lafargue.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
     if (WebSocket) {
-        var socket = new WebSocket('ws://localhost:3001/lafargue/events');
+        var websocket_base = $('head').attr('data-websocket-base');
+        var socket = new WebSocket(websocket_base + '/lafargue/events');
         socket.onmessage = function(event) {
             $('.lafargue-list').prepend(event.data);
         };
