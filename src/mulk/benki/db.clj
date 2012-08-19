a7ca1bad3b36efea5694342c728ff702276e45e6
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-08-19 15:11:30 +0200

    db: Make XA-capable.
    A non-Immutant fallback is provided.

--- src/mulk/benki/db.clj
+++ src/mulk/benki/db.clj
@@ -1,11 +1,48 @@
 (ns mulk.benki.db
   (:refer-clojure)
-  (:use [mulk.benki config])
+  (:use [mulk.benki config]
+        [clojure.pprint :only [cl-format]])
   (:require [clojure.java.jdbc :as sql]))
 
 
+(defn ^:private classic-db []
+  (let [{host :host, port :port, database :database,
+         user :user, password :password}
+        (:database @benki-config)]
+    {:classname "org.postgresql.Driver"
+     :subprotocol "postgresql"
+     :subname (cl-format nil "//~A:~D/~A" host port database)
+     :user user
+     :password password}))
+
+(defonce ^:private +immutant-db+ (atom nil))
+
+(defn ^:private immutant-db []
+  (if @+immutant-db+
+    {:datasource @+immutant-db+}
+    (let [{host :host, port :port, database :database,
+           user :user, password :password}
+          (:database @benki-config)]
+      (when (try
+              (require 'immutant.xa)
+              true
+              (catch Exception e
+                false))
+        (let [datasource (ns-resolve 'immutant.xa 'datasource)]
+          (reset! +immutant-db+
+                  (datasource "benkidb"
+                              {:adapter  "postgres"
+                               :host     host
+                               :port     port
+                               :database database
+                               :username user
+                               :password password
+                               :pool     1}))
+          {:datasource @+immutant-db+})))))
+
 (defn ^:private db []
-  (:database @benki-config))
+  (or (immutant-db) (classic-db)))
+
 
 (defn call-with-db [thunk]
   (sql/with-connection (db)
